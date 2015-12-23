e8d9b760dce9ae30ac22d346f9b9f19b5e6503cc
Christian Grabowski <christiang11754@gmail.com>
2015-12-23 01:27:49 -0500

    use flags

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -8,8 +9,13 @@ import (
 )
 
 func main() {
+	var (
+		platform     = flag.String("-platform", "gcloud", "Set which cloud platform to use, defaults to gcloud")
+		project      = flag.String("-project", "", "Set which project/organization to use, defaults to empty")
+		platConfPath = flag.String("-platform-config", "./config.json", "Set the path to a json config for cloud platform, defaults to ./config.json")
+	)
 	server := http.NewServeMux()
-	mongoHandler := mongo.NewHandler()
+	mongoHandler := mongo.NewHandler(*platform, *project, *platConfPath)
 	server.Handle("/instances", mongoHandler)
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
